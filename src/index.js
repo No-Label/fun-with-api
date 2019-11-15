2361e5fa5a484a4b58d0412eaf3a8ecba73bdd1d
marvingradtke <m.gradtke@gmail.com>
2019-11-15 12:00:05 +0100

    add fetch function

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
- 
+import "babel-polyfill";
+import { getAdvice } from "./advice";
+const randomAdvice = document.querySelector(".random-advice");
+const result = document.querySelector(".advice");
+
+randomAdvice.addEventListener("click", () => {
+  getAdvice().then(data => {
+    console.log(data);
+    const randomAdvice = JSON.stringify(data[0].advice);
+    result.innerHTML = randomAdvice;
+  });
+});
